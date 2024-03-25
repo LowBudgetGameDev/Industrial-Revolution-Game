2fe71f0553cb575dd13cd5ae5b47f4eb940fb922
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2024-03-24 22:22:00 -0400

    made click worker work

--- Scripts/HiredWorkerUI.gd
+++ Scripts/HiredWorkerUI.gd
@@ -4,9 +4,14 @@ class_name HiredWorkerUI
 
 @export var amount_text : RichTextLabel
 @export var worker : Worker
+@export var background : Panel
+@export var click_button : TextureButton
 
 func _ready():
 	WorkerManager.instance.on_hired_worker.connect(self.update_text)
+
+	click_button.pressed.connect(self._on_clicked)
+	move_child(click_button, 999)
 
 func init():
 	update_text()
@@ -21,3 +26,9 @@ func init():
 
 func update_text():
 	amount_text.text = "x" + str(WorkerManager.instance.get_amount_workers_hired(worker))
+
+func _on_clicked():
+	if WorkerManager.instance.get_amount_workers_hired(worker) == 0:
+		return
+
+	background.self_modulate = Color(0.5, 0.5, 0)
