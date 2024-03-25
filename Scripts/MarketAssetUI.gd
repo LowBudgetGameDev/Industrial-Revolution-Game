7fe68ecbddf85341519bd446d12ea4b2d3ef772d
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2024-03-24 22:22:00 -0400

    disabled sell button when have nothing

--- Scripts/MarketAssetUI.gd
+++ Scripts/MarketAssetUI.gd
@@ -10,6 +10,12 @@ class_name MarketAssetUI
 func _ready():
 	sellButton.pressed.connect(self._sell_asset)
 
+func _process(delta):
+	if AssetManager.instance.get_asset_amount(asset) == 0:
+		sellButton.disabled = true
+	else:
+		sellButton.disabled = false
+
 
 func init():
 	assetImage.texture = asset.sprite
