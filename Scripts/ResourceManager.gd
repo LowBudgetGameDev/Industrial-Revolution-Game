57ef0c0b9c98016edeee96fa0aa3d3cd2ff72ea4
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2024-03-24 22:22:00 -0400

    Created Template for Resource UI and Resource Manager.

---
+++ Scripts/ResourceManager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+class_name ResourceManager
+
+enum resource {Wheat, Sugar, Cotton, Coal, Steel, Gold, Paper, Glass, Textile, Brick, Gunpowder, Firearm}
+
+var _resource_amount_dictionary : Dictionary
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
